2e321fb54184aaea32df76d4852b0d4b1d1e7c24
Lee Eunbin <dmsqlsdl54@naver.com>
2024-04-15 14:56:36 +0900

    리뷰 리스트 컴포넌트 작업 완료

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import { useEffect, useState } from "react";
 import ReviewList from "./components/ReviewList/ReviewList";
 import { getReviews } from "./api";
 
+const LIMIT = 6;
+
 const App = () => {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt"); // 정렬 기본 기준 : 최신순
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+  // 현재 네트워크가 request 중이면 true, 아니면 false
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]); // 베스트순 정렬
 
   // 클릭한 기준으로 정렬 변경하는 함수
@@ -21,13 +29,37 @@ const App = () => {
   };
 
   // 영화 리스트 렌더링
-  const handleLoad = async (orderQuery) => {
-    const { reviews } = await getReviews(orderQuery);
-    setItems(reviews);
+  const handleLoad = async (options) => {
+    let result;
+
+    try {
+      setIsLoading(true);
+      setLoadingError(null);
+      result = await getReviews(options);
+    } catch (error) {
+      setLoadingError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+
+    const { reviews, paging } = result;
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems((prevItems) => [...prevItems, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
+  };
+
+  // 다음 페이지를 불러올 함수
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
   };
 
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({ order, offset: 0, limit: LIMIT });
   }, [order]);
 
   return (
@@ -37,6 +69,12 @@ const App = () => {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더 보기
+        </button>
+      )}
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </div>
   );
 };
--- src/api.js
+++ src/api.js
@@ -1,11 +1,18 @@
 /**
  * 리퀘스트 함수들을 모아놓는 함수
  */
-export const getReviews = async (order = "createdAt") => {
-  const query = `order=${order}`;
+export const getReviews = async ({
+  order = "createdAt",
+  offset = 0, // 지금까지 받아온 데이터 개수
+  limit = 6, // 받아올 데이터 개수
+}) => {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
   const response = await fetch(
     `https://learn.codeit.kr/9516/film-reviews?${query}`
   );
+  if (!response.ok) {
+    throw new Error("리뷰를 불러오는데 실패했습니다");
+  }
   const body = response.json();
   return body;
 };
